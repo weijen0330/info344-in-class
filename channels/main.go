756aa4936c24d415ef6d8744e9ec70a17274c5a5
Wei-Jen <weijen0330@hotmail.com>
2017-05-03 13:51:52 -0700

    one long func example

--- channels/main.go
+++ channels/main.go
@@ -1,10 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 //someLongFunc is a function that might
 //take a while to complete, so we want
 //to run it on its own go routine
-func someLongFunc() {
+// channel of ints
+func someLongFunc(ch chan int) {
+	r := rand.Intn(2000)
+	d := time.Duration(r)
+	time.Sleep(time.Millisecond * d)
 
+	// writing r into the channel. Will block if channel is full
+	ch <- r
 }
 
 func main() {
@@ -14,4 +26,13 @@ func main() {
 	//passing the channel so that
 	//someLongFunc() can communicate
 	//its results
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println("starting long-running func...")
+	// unbuffered, accept 1 int at a time
+	ch := make(chan int)
+	go someLongFunc(ch)
+
+	// read out the result
+	result := <-ch
+	fmt.Printf("result was %d\n", result)
 }
